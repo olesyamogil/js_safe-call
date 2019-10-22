a8089fe95586af53aa97bdfab2799259af77bc0b
olesyamogil <olesya.mogil@gmail.com>
2019-10-22 12:58:05 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,43 @@
+// # Safe Call
+//
+// 1. Create a function called `safeCall` that accepts another function `f` as a parameter, calls `f`
+// without any arguments and returns `true` if `f` has executed without any errors or `false` if `f` has thrown an error.
+//     Sample usage:
+//
+//     ```js
+//     safeCall(() => console.log('Hello!')); // true
+//     safeCall(() => JSON.parse('abc')); // false
+//     safeCall(() => false); // true
+//     safeCall(() => abc); // false
+//     ```
+//
+// 2. (optional) Make `safeCall` callable on any function: `f.safeCall(...)`. It should take the same parameters
+// and has the same effect as `f.call(...)`, except in case when `f` throws an error,
+//     it should instead just return `null` without throwing any errors.
+
+Object.prototype.safeCallMethod = function(){
+    try {
+        return this.call();
+    } catch (e) {
+        return null;
+    }
+};
+
+
+const f = () => abc;
+console.log(f.safeCallMethod());
+
+const safeCall = function(f){
+    try {
+        f();
+        return true;
+    } catch (e) {
+        return false;
+    } finally {
+        console.log("safeCall completed");
+    }
+};
+console.log(safeCall(() => console.log('Hello!')));
+console.log(safeCall(() => JSON.parse('abc')));
+console.log(safeCall(() => false));
+console.log(safeCall(() => abc));
